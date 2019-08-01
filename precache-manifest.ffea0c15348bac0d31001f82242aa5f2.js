c91f7dd820af9a8a24a4f8ceb27bfd683cf5043a
NunyaBizness <jmclark801@yahoo.com>
2019-07-31 19:34:17 -0600

    Updates

---
+++ precache-manifest.ffea0c15348bac0d31001f82242aa5f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df919d2c318f7829dadfb9accf1862e",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "c52081d330c12282e75a",
+    "url": "/Clicky-Game/static/css/main.7d9f1135.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Clicky-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c52081d330c12282e75a",
+    "url": "/Clicky-Game/static/js/main.1be6c187.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
