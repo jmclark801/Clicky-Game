5b054e8cd81ff8ce97a9dd093c273e32e4e3c5e8
NunyaBizness <jmclark801@yahoo.com>
2019-07-31 19:22:07 -0600

    fixed everything thankfully

---
+++ clicky-game/build/precache-manifest.c4606554ee8a5ce801732c8df3147f06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e3fd427851d13298af48f33ba02e925",
+    "url": "/jmclark801/Clicky-Game/index.html"
+  },
+  {
+    "revision": "c52081d330c12282e75a",
+    "url": "/jmclark801/Clicky-Game/static/css/main.7d9f1135.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/jmclark801/Clicky-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c52081d330c12282e75a",
+    "url": "/jmclark801/Clicky-Game/static/js/main.1be6c187.chunk.js"
+  },
+  {
+    "revision": "baa034f33f1953fa600f",
+    "url": "/jmclark801/Clicky-Game/static/js/runtime~main.e87ca770.js"
+  }
+]);
